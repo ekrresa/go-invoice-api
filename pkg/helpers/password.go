60907307c18a02ce996c34450bc60b2f270c86e1
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2023-12-24 17:01:11 +0100

    Reduce bcrypt cost

--- pkg/helpers/password.go
+++ pkg/helpers/password.go
@@ -3,7 +3,7 @@ package helpers
 import "golang.org/x/crypto/bcrypt"
 
 func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 16)
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 12)
 	return string(bytes), err
 }
 
